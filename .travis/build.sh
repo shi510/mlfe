d2151f250ffee3d52a838cf63f9a21cf13d2f900
SungHoon Im <lilipopogm@gmail.com>
2017-11-06 06:05:59 +0900

    add travis ci files

---
+++ .travis/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+
+LOCAL_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT_DIR=$(dirname "$LOCAL_DIR")
+cd "$ROOT_DIR"
+
+mkdir build
+cd build
+
+CMAKE_ARGS=('-DCMAKE_BUILD_TEST=ON')
+CMAKE_ARGS+=('-DCMAKE_INSTALL_PREFIX=../install')
+cmake .. ${CMAKE_ARGS[*]}
+if [ "$TRAVIS_OS_NAME" = 'linux' ]; then
+    make "-j$(nproc)" install
+fi
