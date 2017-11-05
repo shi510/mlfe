761e1b3df2b1dfa2f3b7d4f565d18041149595e7
SungHoon Im <lilipopogm@gmail.com>
2017-11-06 01:53:58 +0900

    add mnist data downloader. the downloader requires python, pip, wgetter, gzip. this code automatically download and unzip.

---
+++ data/mnist/get_mnist.py
@@ -0,0 +1,16 @@
+import wgetter, gzip
+
+file_names = [
+                'train-images-idx3-ubyte', 
+                'train-labels-idx1-ubyte', 
+                't10k-images-idx3-ubyte', 
+                't10k-labels-idx1-ubyte'
+            ]
+
+for i in range(0, len(file_names)):
+    fileName = wgetter.download('http://yann.lecun.com/exdb/mnist/%s.gz' % file_names[i])
+    inF = gzip.open(file_names[i] + '.gz', 'rb')
+    outF = open(file_names[i], 'wb')
+    outF.write(inF.read())
+    inF.close()
+    outF.close()
