b0c0b17332e8fb2067a0f502751ec2f1ed6c666a
SungHoon Im <lilipopogm@gmail.com>
2017-11-06 06:40:36 +0900

    override script and make install only on linux.

--- .travis/build.sh
+++ .travis/build.sh
@@ -11,7 +11,7 @@ cd build
 CMAKE_ARGS=('-DBUILD_TEST=ON')
 CMAKE_ARGS+=('-DCMAKE_INSTALL_PREFIX=../install')
 cmake .. ${CMAKE_ARGS[*]}
-make "-j$(nproc)" install
-# if [ "$TRAVIS_OS_NAME" = 'linux' ]; then
-#     make "-j$(nproc)" install
-# fi
+
+if [ "$TRAVIS_OS_NAME" = 'linux' ]; then
+    make "-j$(nproc)" install
+fi
