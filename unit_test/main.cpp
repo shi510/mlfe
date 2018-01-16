2f37d36293f3c0e0c0cbbb8c3564519a77bc7dc0
SungHoon Im <lilipopogm@gmail.com>
2018-01-16 23:19:04 +0900

    remove header include line.

--- unit_test/main.cpp
+++ unit_test/main.cpp
@@ -1,8 +1,4 @@
 #include <gtest/gtest.h>
-#include "test_fc.hpp"
-#include "test_softmax_xent.hpp"
-#include "test_simpledb.hpp"
-#include "test_conv.hpp"
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
