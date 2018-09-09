9fb7ca5362008a31378438142f518a976353bc84
SungHoon Im <lilipopogm@gmail.com>
2018-09-10 00:43:28 +0900

    add compregensive headers.

---
+++ mlfe/operators.h
@@ -0,0 +1,13 @@
+#ifndef __OPERATORS_H__
+#define __OPERATORS_H__
+#include "operators/activations.h"
+#include "operators/convolution.h"
+#include "operators/cross_entropy.h"
+#include "operators/dense.h"
+#include "operators/initializer.h"
+#include "operators/pool.h"
+#include "operators/reshape.h"
+#include "operators/reduce.h"
+#include "operators/dropout.h"
+
+#endif // end #ifndef __OPERATORS_H__
---
+++ mlfe/optimizers.h
@@ -0,0 +1,7 @@
+#ifndef __OPTIMIZERS_H__
+#define __OPTIMIZERS_H__
+#include "optimizers/gradient_descent.h"
+#include "optimizers/adadelta.h"
+#include "optimizers/adam.h"
+
+#endif // end #ifndef __OPTIMIZERS_H__
