ff2958bb82bee3df17e6cc9f29f5b920399cdbe6
SungHoon Im <lilipopogm@gmail.com>
2018-11-20 15:35:30 +0900

    add gradient unit test as below: grad of negative op. grad of sigmoid op. grad of relu op.

--- unit_test/unary_op_test.cc
+++ unit_test/unary_op_test.cc
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <mlfe/core.h>
 #include <mlfe/operators.h>
+#include <mlfe/utils/gradient_checker.h>
+#include <random>
 
 using namespace mlfe;
 namespace fn = functional;
@@ -18,6 +20,31 @@ TEST(unary_op, negative){
     EXPECT_EQ(result.data<T>()[1], -var.data<T>()[1]);
     EXPECT_EQ(result.data<T>()[2], -var.data<T>()[2]);
     EXPECT_EQ(result.data<T>()[3], -var.data<T>()[3]);
+}
+
+TEST(unary_op, negative_grad){
+    using T = float;
+    constexpr T grad_eps = 1e-4;
+    constexpr T pass_eps = 1e-3;
+    auto var = fn::create_variable({2, 2});
+    auto result = fn::negative(var);
+    auto analytical = std::vector<T>(var.size());
+    std::mt19937 rng;
+    std::uniform_real_distribution<T> dist(-1, 1);
+    
+    std::generate(var.begin<T>(), var.end<T>(), [&rng, &dist](){
+        return dist(rng);
+    });
+    result.eval();
+    result.backprop();
+    std::copy(var.grad().begin<T>(), var.grad().end<T>(), analytical.begin());
+    auto numerical = numerical_gradient(grad_eps, result, var);
+    
+    for(int n = 0; n < var.size(); ++n){
+        auto diff = std::abs(analytical.data()[n] - numerical.data<T>()[n]);
+        EXPECT_LE(diff, pass_eps);
+        EXPECT_GE(diff, -pass_eps);
+    }
 }
 
 TEST(unary_op, sigmoid){
@@ -41,6 +68,32 @@ TEST(unary_op, sigmoid){
     EXPECT_GE(result.data<T>()[3], 0.7311 - 1e-4);
 }
 
+TEST(unary_op, sigmoid_grad){
+    using T = float;
+    constexpr T grad_eps = 1e-4;
+    constexpr T pass_eps = 1e-3;
+    auto var = fn::create_variable({2, 2});
+    auto result = fn::sigmoid(var);
+    auto analytical = std::vector<T>(var.size());
+    std::mt19937 rng;
+    std::uniform_real_distribution<T> dist(-1, 1);
+    rng.seed(std::random_device()());
+    
+    std::generate(var.begin<T>(), var.end<T>(), [&rng, &dist](){
+        return dist(rng);
+    });
+    result.eval();
+    result.backprop();
+    std::copy(var.grad().begin<T>(), var.grad().end<T>(), analytical.begin());
+    auto numerical = numerical_gradient(grad_eps, result, var);
+    
+    for(int n = 0; n < var.size(); ++n){
+        auto diff = std::abs(analytical.data()[n] - numerical.data<T>()[n]);
+        EXPECT_LE(diff, pass_eps);
+        EXPECT_GE(diff, -pass_eps);
+    }
+}
+
 TEST(unary_op, relu){
     using T = float;
     auto var = fn::create_variable({2, 2});
@@ -54,4 +107,29 @@ TEST(unary_op, relu){
     EXPECT_EQ(result.data<T>()[1], var.data<T>()[1]);
     EXPECT_EQ(result.data<T>()[2], 0);
     EXPECT_EQ(result.data<T>()[3], var.data<T>()[3]);
+}
+
+TEST(unary_op, relu_grad){
+    using T = float;
+    constexpr T grad_eps = 1e-4;
+    constexpr T pass_eps = 1e-3;
+    auto var = fn::create_variable({2, 2});
+    auto result = fn::relu(var);
+    auto analytical = std::vector<T>(var.size());
+    std::mt19937 rng;
+    std::uniform_real_distribution<T> dist(-1, 1);
+    
+    std::generate(var.begin<T>(), var.end<T>(), [&rng, &dist](){
+        return dist(rng);
+    });
+    result.eval();
+    result.backprop();
+    std::copy(var.grad().begin<T>(), var.grad().end<T>(), analytical.begin());
+    auto numerical = numerical_gradient(grad_eps, result, var);
+    
+    for(int n = 0; n < var.size(); ++n){
+        auto diff = std::abs(analytical.data()[n] - numerical.data<T>()[n]);
+        EXPECT_LE(diff, pass_eps);
+        EXPECT_GE(diff, -pass_eps);
+    }
 }
