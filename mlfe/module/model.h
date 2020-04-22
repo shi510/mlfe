91ba063f65264e3482ba0fd95d34f8da7ba2f194
Im Sunghoon <shi510.kor@gmail.com>
2020-04-22 12:29:47 +0900

    Split model class.

--- mlfe/module/model.h
+++ mlfe/module/model.h
@@ -112,33 +112,5 @@ private:
 	std::shared_ptr<opt::optimizer> __optim;
 };
 
-model::model(Tensor input, Tensor output, std::string name)
-{
-	__input = input;
-	__output = output;
-	__true = functional::create_variable(__output.shape());
-}
-
-void model::compile(
-	std::shared_ptr<opt::optimizer> optim,
-	LossFn loss_fn,
-	MetricFn metric_fn)
-{
-	__loss_fn = loss_fn;
-	__optim = optim;
-	__metric_fn = metric_fn;
-	__loss = functional::mean(__loss_fn(__output, __true));
-	__loss.set_name("loss");
-	__output.set_name("output");
-	__true.set_name("true");
-	for (auto& var : visit_bfs(__loss))
-	{
-		if (var.trainable())
-		{
-			__train_vars.push_back(var);
-		}
-	}
-}
-
 } // end namespace module
 } // end namespace mlfe
