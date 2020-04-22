862668fe623507ce63046463ddba17bf63ed696d
Im Sunghoon <shi510.kor@gmail.com>
2020-04-22 17:57:17 +0900

    add travis script.

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,14 +4,8 @@ set -ex
 LOCAL_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT_DIR=$(dirname "$LOCAL_DIR")
 cd "$ROOT_DIR"
-
-mkdir build
 cd build
 
-CMAKE_ARGS=('-DBUILD_TEST=OFF')
-CMAKE_ARGS+=('-DCMAKE_INSTALL_PREFIX=../install')
-cmake .. ${CMAKE_ARGS[*]}
-
 if [ "$TRAVIS_OS_NAME" = 'linux' ]; then
-    make "-j$(nproc)"
+    make install
 fi
