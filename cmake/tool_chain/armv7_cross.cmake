0ff2342533399f7b327ec50d82bbf230bb85d2e9
Im Sunghoon <shi510.kor@gmail.com>
2020-07-20 04:01:33 +0000

    Fix typo of C compiler name in armv7 script.

--- cmake/tool_chain/armv7_cross.cmake
+++ cmake/tool_chain/armv7_cross.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR armv7)
 
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-g++-8)
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc-8)
 set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++-8)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
