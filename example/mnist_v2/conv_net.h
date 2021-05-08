a7592a08c3371bf90874789d8c66e29937d7d6a4
Im Sunghoon <shi510.kor@gmail.com>
2021-05-08 23:39:57 +0900

    Update example

--- example/mnist_v2/conv_net.h
+++ example/mnist_v2/conv_net.h
@@ -2,48 +2,50 @@
 #include <mlfe/core/tensor.h>
 #include <mlfe/operators_v2/reduce_mean.h>
 #include <mlfe/operators_v2/softmax_cross_entropy.h>
-#include <mlfe/operators_v2/matmul.h>
-#include <mlfe/operators_v2/conv2d.h>
 #include <mlfe/operators_v2/maxpool2d.h>
 #include <mlfe/operators_v2/relu.h>
+#include <mlfe/nn/layers/linear.h>
+#include <mlfe/nn/layers/conv2d.h>
+#include <mlfe/nn/module.h>
 
 namespace models{
 using namespace mlfe;
-namespace v2 = mlfe::operators_v2;
-namespace fn = mlfe::functional;
-/*
+namespace op = mlfe::operators_v2;
 
 struct mnist_conv_net : nn::module{
     nn::conv2d conv1;
     nn::conv2d conv2;
-    nn::dropout dropout1;
-    nn::dropout dropout2;
+    nn::conv2d conv3;
     nn::linear fc1;
     nn::linear fc2;
 
     mnist_conv_net(){
-        conv1 = trainable(nn::conv2d(3, 32, {3, 3}, true));
-        conv2 = trainable(nn::conv2d(32, 64, {3, 3}, true));
-        dropout1 = nn::dropout(0.25);
-        dropout2 = nn::dropout(0.5);
-        fc1 = trainable(nn::linear());
-        fc2 = trainable(nn::linear());
+        conv1 = trainable(nn::conv2d(1, 12, {3, 3}, {1, 1}, true));
+        conv2 = trainable(nn::conv2d(12, 24, {3, 3}, {1, 1}, true));
+        fc1 = trainable(nn::linear(7*7*24, 64));
+        fc2 = trainable(nn::linear(64, 10));
     }
 
-    tensor forward(tensor x){
+    Tensor forward(Tensor x){
         x = conv1(x);
-        x = relu(x);
-        x = dropout1(x);
-        x = maxpool2d(x, {2, 2}, {1, 1});
+        x = op::maxpool2d(x, {2, 2}, {2, 2});
+        x = op::relu(x);
         x = conv2(x);
-        x = relu(x);
-        x = dropout2(x);
-        x = maxpool2d(x, {2, 2}, {1, 1});
+        x = op::maxpool2d(x, {2, 2}, {2, 2});
+        x = op::relu(x);
+        x = x.view({x.shape()[0], 7 * 7 * 24});
         x = fc1(x);
+        x = op::relu(x);
         x = fc2(x);
         return x;
     }
+
+    Tensor criterion(Tensor y_true, Tensor y_pred)
+    {
+        auto loss = op::softmax_cross_entropy(y_true, y_pred);
+        loss = op::reduce_mean(loss);
+        return loss;
+    }
 };
-*/
 
 } // namespace models
