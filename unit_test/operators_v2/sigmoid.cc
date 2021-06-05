73c627fe9e8bb8e92ac243c0718256824a15f0ca
Im Sunghoon <shi510.kor@gmail.com>
2021-06-06 00:20:30 +0900

    Add unit test for sigmoid op

--- unit_test/operators_v2/sigmoid.cc
+++ unit_test/operators_v2/sigmoid.cc
@@ -1,23 +1,34 @@
 #include <gtest/gtest.h>
-#include <mlfe/operators_v2/relu.h>
+#include <mlfe/core.h>
+#include <mlfe/core/tensor.h>
+#include <mlfe/operators_v2/sigmoid.h>
 #include <mlfe/utils/gradient_checker.h>
 #include <random>
+#include <numeric>
+
 
 using namespace mlfe;
-using namespace mlfe::operators_v2;
-namespace fn = mlfe::functional;
+namespace fn = functional;
+namespace op = operators_v2;
 
-TEST(operator_v2, relu){
+TEST(operator_v2, sigmoid){
     using T = float;
-    auto input = Tensor::from_vector<T>({-1.35f, 357.5f, -3.5f, 3.15f}, {2, 2});
-    auto result = relu(input);
-    EXPECT_EQ(result.data<T>()[0], 0);
-    EXPECT_EQ(result.data<T>()[1], input.data<T>()[1]);
-    EXPECT_EQ(result.data<T>()[2], 0);
-    EXPECT_EQ(result.data<T>()[3], input.data<T>()[3]);
+    auto var = Tensor::from_vector<T>({0, -0.5, 0.5, 1}, {2, 2});
+    auto result = op::sigmoid(var);
+
+    EXPECT_EQ(result.data<T>()[0], 0.5);
+
+    EXPECT_LE(result.data<T>()[1], 0.3775 + 1e-4);
+    EXPECT_GE(result.data<T>()[1], 0.3775 - 1e-4);
+
+    EXPECT_LE(result.data<T>()[2], 0.6225 + 1e-4);
+    EXPECT_GE(result.data<T>()[2], 0.6225 - 1e-4);
+
+    EXPECT_LE(result.data<T>()[3], 0.7311 + 1e-4);
+    EXPECT_GE(result.data<T>()[3], 0.7311 - 1e-4);
 }
 
-TEST(operator_v2, relu_grad){
+TEST(operator_v2, sigmoid_grad){
     using T = float;
     constexpr T grad_eps = 1e-4;
     constexpr T pass_eps = 1e-3;
@@ -29,11 +40,11 @@ TEST(operator_v2, relu_grad){
     std::generate(x.begin<T>(), x.end<T>(), [&rng, &dist](){
         return dist(rng);
     });
-    auto result = relu(x);
+    auto result = op::sigmoid(x);
     result.backprop_v2();
     std::copy(x.grad_v2().begin<T>(), x.grad_v2().end<T>(), analytical.begin<T>());
     auto func = [](mlfe::Tensor& x){
-        return relu(x);
+        return op::sigmoid(x);
     };
     auto numerical = numerical_gradient_v2(func, x, grad_eps);
     auto diff = calculate_gradient_diff<T>(numerical, analytical);
