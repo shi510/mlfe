d593370ce68f1f2b020105c21751c0823699cc7a
Im Sunghoon <shi510.kor@gmail.com>
2021-06-07 01:21:00 +0900

    Move matmul cublas impl

--- mlfe/operators_v2/impl/cuda/matmul.cc
+++ mlfe/operators_v2/impl/cuda/matmul.cc
@@ -1,11 +1,47 @@
 #include "mlfe/operators_v2/matmul.h"
 #include "mlfe/core/op_kernel.h"
 #include "mlfe/device_context/cuda_context.h"
-#include "mlfe/math/blas.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#include <iostream>
 
 namespace mlfe{
 namespace operators_v2{
 namespace{
+
+template<typename T>
+void gemm(
+    const bool trans_a,
+    const bool trans_b,
+    const int m,
+    const int n,
+    const int k,
+    const T alpha,
+    const T *a_ptr,
+    const int lda,
+    const T *b_ptr,
+    const int ldb,
+    const T beta,
+    T *c_ptr,
+    const int ldc
+    )
+{
+    auto cuTransA = !trans_a ? CUBLAS_OP_N : CUBLAS_OP_T;
+    auto cuTransB = !trans_b ? CUBLAS_OP_N : CUBLAS_OP_T;
+    auto hdl = cuda_context_v2::create()->get_cublas_handle();
+    auto status = cublasSgemm(
+        hdl,
+        cuTransB, cuTransA,
+        n, m, k,
+        &alpha, b_ptr, (!trans_b) ? n : k,
+        a_ptr, (!trans_a) ? k : m,
+        &beta, c_ptr, n);
+    if (status != CUBLAS_STATUS_SUCCESS)
+    {
+        std::cout<<"[Warnning cublas] cublasSgemm failed."<<std::endl;
+    }
+}
 
 template <typename T>
 void matmul_fwd_impl(
@@ -22,13 +58,12 @@ void matmul_fwd_impl(
     auto a_ptr = a.device_data<T>();
     auto b_ptr = b.device_data<T>();
     auto y_ptr = y.mutable_device_data<T>();
-    CUDAContext c;
-    math::gemm<T, CUDAContext>(
-        transpose_a, transpose_b,
+
+    gemm<T>(transpose_a, transpose_b,
         m, n, k,
         T(1), a_ptr, a.shape()[1],
         b_ptr, b.shape()[1],
-        T(0), y_ptr, y.shape()[1], &c);
+        T(0), y_ptr, y.shape()[1]);
 }
 
 } // namespace anonymous
