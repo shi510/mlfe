aec8d52ddb54ce6f6e00a6515d7023ffba9d02f0
Im Sunghoon <shi510.kor@gmail.com>
2021-08-20 21:36:52 +0900

    Fix something wrong
    1. Changed mnist url to mirror of google.
    2. Removed CUDA gencode, now set default.

--- data/mnist/get_mnist.py
+++ data/mnist/get_mnist.py
@@ -8,7 +8,7 @@ file_names = [
             ]
 
 for i in range(0, len(file_names)):
-    fileName = wgetter.download('http://yann.lecun.com/exdb/mnist/%s.gz' % file_names[i])
+    fileName = wgetter.download('https://storage.googleapis.com/cvdf-datasets/mnist/%s.gz' % file_names[i])
     inF = gzip.open(file_names[i] + '.gz', 'rb')
     outF = open(file_names[i], 'wb')
     outF.write(inF.read())
